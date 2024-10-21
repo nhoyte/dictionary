71b6e6608ee0b15c2b4de7feec8d6685091780b2
Novia H <noviahoyte@gmail.com>
2024-10-20 20:34:15 -0400

    Installed Axios. Updated Search.js component to include SheCodes Dictionary API integration.

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import axios from "axios";
+
 import "./Search.css";
 
 export default function Search() {
@@ -7,13 +9,22 @@ export default function Search() {
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${term} definition 🔍`);
+    searchTerm(term);
   }
 
   function updateTerm(event) {
     event.preventDefault();
     setTerm(event.target.value);
   }
-
+  //Fetch response from SheCodes API: https://www.shecodes.io/learn/apis/dictionary
+  function searchTerm(term) {
+    let apiKey = "b00377005017b9aacft302b5od1aa426";
+    let url = `https://api.shecodes.io/dictionary/v1/define?word=${term}&key=${apiKey}`;
+    axios.get(url).then(handleResponse);
+  }
+  function handleResponse(response) {
+    console.log(response.data);
+  }
   return (
     <div className="Search container">
       <form onSubmit={search}>
